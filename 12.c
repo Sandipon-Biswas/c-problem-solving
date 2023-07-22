5e5dd11180d3279bdddbf38b34857fa563c6ea2f
Sandipon-Biswas <98538000+Sandipon-Biswas@users.noreply.github.com>
2023-07-22 18:50:12 +0600

    ternary

--- 12.c
+++ 12.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 void main()
  {
-float weight1,weight2,result;
-int quantity1,quantity2;
-printf("Enter the item1 weight and quantity in single line :");
-scanf("%f %d",&weight1,&quantity1);
-printf("Enter the item2 weight and quantity in single line :");
-scanf("%f %d",&weight2,&quantity2);
-
-result=(weight1*quantity1+weight2*quantity2)/(quantity1+quantity2);
-printf("The result is: %f\n", result);
+int a,b,c;
+printf("Enter three intiger number: ");
+scanf("%d%d%d",&a,&b,&c);
+(a>b && a>c)?printf("the larges number is b %d",a) : (b>a&&b>c) ?printf("the larges number is b %d",b):printf("the largest number is c%d",c);
 
 }
