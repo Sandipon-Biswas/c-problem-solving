7fb8d143601a52befaf1504f464b4fa5ee5e7097
Sandipon-Biswas <98538000+Sandipon-Biswas@users.noreply.github.com>
2023-10-25 23:34:15 +0600

    datatype

---
+++ cpp/datatype.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std; 
+  
+int main() {
+    int i;
+    long long ll;
+    char ch;
+    float f;
+    double d;
+
+    cin >> i >> ll >> ch >> f >> d;
+
+  
+    cout << i << endl;
+    cout << ll << endl;
+    cout << ch << endl;
+    
+  
+    cout << fixed << setprecision(2) << f << endl;
+
+
+    cout << fixed << setprecision(1) << d << endl;
+
+    return 0;
+}
