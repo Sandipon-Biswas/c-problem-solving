d504e6db559fddbc63b67d5c91d258a11659a484
Sandipon-Biswas <98538000+Sandipon-Biswas@users.noreply.github.com>
2024-03-23 13:31:27 +0600

    cpp setup

---
+++ 1CppInpUtoutputSetup/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+  ll tt;
+  cin>>tt;
+  while(tt--){
+           ll u,v;
+    cin>>u>>v;
+    cout << -(u * u) << " " << v * v << '\n';
+  }
+  return 0;
+}
